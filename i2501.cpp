f5babb8636a927fab78d7e77da52afc0d098be99
tle204 <143390902+tle204@users.noreply.github.com>
2023-08-28 22:30:22 +0700

    Create i2501.cpp

---
+++ i2501.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+int n;
+scanf("%d",&n);
+if(n==1){
+    printf("Thang Mot\n");
+}
+else if(n==2){
+    printf("Thang Hai\n");
+}
+else if(n==3){
+    printf("Thang Ba\n");
+}
+else if(n==4){
+    printf("Thang Tu\n");
+}
+else if(n==5){
+    printf("Thang Nam\n");
+}
+else if(n==6){
+    printf("Thang Sau\n");
+}
+else if(n==7){
+    printf("Thang Bay\n");
+}
+else if(n==8){
+    printf("Thang Tam\n");
+}
+else if(n==9){
+    printf("Thang Chin\n");
+}
+else if(n==10){
+    printf("Thang Muoi\n");
+}
+else if(n==11){
+    printf("Thang Muoi mot\n");
+}
+else if(n==12){
+    printf("Thang Muoi hai\n");
+}
+else 
+printf("Khong co");
+}
