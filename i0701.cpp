b7671d472526dd3997223cfff6725d1c023e8811
tle204 <143390902+tle204@users.noreply.github.com>
2023-08-28 22:40:31 +0700

    Create i0701.cpp

---
+++ i0701.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if (n/2==0)
+    printf("%d\n%d\n%d\n%d\n%d\n%d",n+1,n+3,n+5,n+7,n+9,n+11);
+    else
+    printf("%d\n%d\n%d\n%d\n%d\n%d",n,n+2,n+4,n+6,n+8,n+10);
+}
