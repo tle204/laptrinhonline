15d9a289e3f859254b5aab87dab668bf592526e2
tle204 <143390902+tle204@users.noreply.github.com>
2023-08-28 22:54:16 +0700

    Create i4111.cpp

---
+++ i4111.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int p;
+	for (int i=1;; i++) {
+		scanf("%d",&p);
+		if(p==2002) {
+			printf("Duoc phep truy cap");
+			break;
+		} else
+			printf("Sai mat khau\n");
+	}
+}
