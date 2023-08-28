d12d9e4fefcdcddaf2ca97b215a5ba6bb49107de
tle204 <143390902+tle204@users.noreply.github.com>
2023-08-28 23:47:14 +0700

    Create i9411.cpp

---
+++ i9411.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+int main() {
+	int a,b,s=0;
+	scanf("%d",&a);
+	do {
+		scanf("%d",&b);
+		if(b>0) {
+			break;
+		}
+	} while(b<=0);
+	for(int i=a; i<a+b; i++) {
+		s = s+i;
+	}
+	printf("%d",s);
+}
