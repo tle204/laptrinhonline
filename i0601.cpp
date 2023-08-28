cc6530ce30f6d79b8bc4ca5726629ebac6605ebf
tle204 <143390902+tle204@users.noreply.github.com>
2023-08-28 22:31:28 +0700

    Create i0601.cpp

---
+++ i0601.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+int main()
+{
+    int a[1000],dem=0;
+    for (int i=1;i<=6;++i)
+    {
+    	scanf("%d",&a[i]);
+    	if (a[i]>0)
+    	++dem;
+	}
+	printf ("%d",dem);
+	printf(" so duong");
+}
