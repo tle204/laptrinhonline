8b5a9ef69c48346575e106f5b988550cf53cb2f6
TuanLe <143390902+tle204@users.noreply.github.com>
2023-08-29 18:04:55 +0700

    Create mah2.cpp

---
+++ mah2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define M 102
+
+int n,x[M],d=0;
+bool cot[M],cheo1[M],cheo2[M];
+void qlhoanvi(int i) {
+	for(int j=1; j<=n; j++)
+		if(!cot[j] && !cheo1[i-j+n] && !cheo2[i+j-1]) {
+			x[i]=j;
+			cot[j]=true;
+			cheo1[i-j+n]=true;
+			cheo2[i+j-1]=true;
+			if(i==n)
+				d++;
+			else
+				qlhoanvi(i+1);
+			cot[j]=false;
+			cheo1[i-j+n]=false;
+			cheo2[i+j-1]=false;
+
+		}
+}
+int main() {
+	cin>>n;
+	qlhoanvi(1);
+	cout<<d<<endl;
+	return 0;
+}
