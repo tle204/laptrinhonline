cea884592610e4f52e328a6350e4c4dd15d639cd
TuanLe <143390902+tle204@users.noreply.github.com>
2023-08-29 17:56:14 +0700

    Create i3842.cpp

---
+++ i3842.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std ;
+int main () {
+	long long int x;
+	scanf("%lld",&x);
+	printf("Feliz nat");
+	for(int i=1;i<=x;i++)
+	printf("a");
+	printf("l!");
+}
