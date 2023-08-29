c1be438dee04e996f8cb9112f6763aec50faff56
TuanLe <143390902+tle204@users.noreply.github.com>
2023-08-29 17:00:53 +0700

    Create i4651.cpp

---
+++ i4651.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    for(int i=0;;i++){
+        int X,s=0;
+        cin>>X;
+        if(getchar() == EOF) break;
+        else{
+            if(X==0){
+                cout<<"Khong khieu nai!"<<"\n";
+            }
+            else cout<<"Co khieu nai!"<<"\n";
+        }
+    }
+
+}
